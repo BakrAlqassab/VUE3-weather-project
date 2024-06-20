66814e1563bb966bd0491f12c023dca96126feda
bakr <alqassab31@gmail.com>
2024-06-20 12:31:21 +0300

    add the basics

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -5,8 +5,8 @@ export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
   function increment() {
-    count.value++
-  }
+    count.value++ 
+  } 
 
   return { count, doubleCount, increment }
 })
